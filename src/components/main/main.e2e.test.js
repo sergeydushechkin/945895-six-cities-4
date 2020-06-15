3a96819ddd36fa6e8961c649457df4048fe6ec82
Sergey <dushechkin_serge@mail.ru>
2020-06-15 14:49:01 +0300

    Добавит e2e тест

---
+++ src/components/main/main.e2e.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Main from "./main.jsx";
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+it(`Should header to be pressed`, () => {
+  const onPlaceCardHeaderClick = jest.fn();
+
+  const main = shallow(
+      <Main
+        rentsCount = {312}
+        cardsNames = {[`Beautiful & luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`]}
+        onPlaceCardHeaderClick = {onPlaceCardHeaderClick}
+      />
+  );
+
+  const placeCardHeader = main.find(`.place-card__name a`).at(1);
+
+  placeCardHeader.props().onClick();
+
+  expect(onPlaceCardHeaderClick.mock.calls.length).toBe(1);
+});
