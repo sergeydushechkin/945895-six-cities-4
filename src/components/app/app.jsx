41b907b388db22cc50d12f214da16eba8a8d422a
Sergey <dushechkin_serge@mail.ru>
2020-06-19 12:52:05 +0300

    Добавит в проект react-router-dom, подключит в app.jsx

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 
-const App = (props) => {
-  const {rentsCount, offers} = props;
+class App extends React.PureComponent {
 
-  return (
-    <Main
-      rentsCount={rentsCount}
-      offers = {offers}
-    />
-  );
-};
+  render() {
+    const {rentsCount, offers} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main
+              rentsCount={rentsCount}
+              offers = {offers}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   rentsCount: PropTypes.number.isRequired,
