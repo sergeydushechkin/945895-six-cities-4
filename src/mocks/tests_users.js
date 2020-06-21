d4de7625e790ecf74962dde4a430a007ea45e115
Sergey <54816584+sergeydushechkin@users.noreply.github.com>
2020-06-21 20:26:08 +0300

    Добавит общую функцию для расчета рейтинга. Добавит поля в моки. Добавит моки с пользователями.

---
+++ src/mocks/tests_users.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    name: `Angelina`,
+    super: true,
+    avatar: `img/avatar-angelina.jpg`
+  },
+  {
+    id: 2,
+    name: `Max`,
+    super: false,
+    avatar: `img/avatar-max.jpg`
+  },
+  {
+    id: 3,
+    name: `Ivan`,
+    super: false,
+    avatar: `img/avatar-max.jpg`
+  },
+  {
+    id: 4,
+    name: `Inna`,
+    super: true,
+    avatar: `img/avatar-angelina.jpg`
+  },
+  {
+    id: 5,
+    name: `Sergey`,
+    super: true,
+    avatar: `img/avatar-max.jpg`
+  }
+];
