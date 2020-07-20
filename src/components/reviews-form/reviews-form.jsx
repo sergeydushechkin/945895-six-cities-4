5ab080e8b492530b24f12a13d97cce3117e88b78
Sergey <dushechkin_serge@mail.ru>
2020-07-20 17:55:00 +0300

    Добавит валидацию, обработку ошибки и тестовую отправку формы

--- src/components/reviews-form/reviews-form.jsx
+++ src/components/reviews-form/reviews-form.jsx
@@ -1,95 +1,135 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, {createRef} from "react";
+// import PropTypes from "prop-types";
+
+const debugFunc = (() => {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      const result = Math.random() > 0.5;
+      if (result) {
+        resolve(result);
+      } else {
+        reject(result);
+      }
+    }, 1000);
+  });
+});
 
 class ReviewsForm extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      rating: null,
+      rating: ``,
       text: ``,
-      submitState: false,
+      isSubmitDisabled: true,
+      isFormDisabled: false,
+      errorText: ``,
     };
+
+    this.formRef = createRef();
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(evt) {
-    const element = evt.target;
-    const {text, rating} = this.state;
+  _resetFromState() {
+    this.formRef.current.reset();
 
-    switch (element.name) {
+    this.setState({
+      rating: ``,
+      text: ``,
+      isSubmitDisabled: true,
+      isFormDisabled: false,
+      errorText: ``,
+    });
+  }
+
+  handleChange(evt) {
+    const value = evt.target.value;
+    let {text, rating} = this.state;
+
+    switch (evt.target.name) {
       case `rating`:
-        this.setState({rating: element.value});
+        this.setState({rating: value});
+        rating = value;
         break;
       case `review`:
-        this.setState({text: element.value});
+        this.setState({text: value});
+        text = value;
     }
 
-    // this.setState({submitState: (this.state.rating && (this.state.text.length >= 50 && this.state.text.length <= 300))});
+    console.log(text, rating);
 
-    if (rating && (text.length >= 50 && text.length <= 300)) {
-      this.setState({submitState: true});
-    } else {
-      this.setState({submitState: false});
-    }
+    this.setState({isSubmitDisabled: !(rating && (text.length >= 50 && text.length <= 300))});
   }
 
   handleSubmit(evt) {
-    evt.preventDefault();
+    const func = debugFunc();
 
+    evt.preventDefault();
+    this.setState({isFormDisabled: true, isSubmitDisabled: true});
+
+    func
+      .then(() => {
+        this._resetFromState();
+      })
+      .catch((result) => {
+        this.setState({isFormDisabled: false, isSubmitDisabled: false, errorText: `Error: ${result}`});
+      });
   }
 
   render() {
-    const {submitState} = this.state;
+    const {isSubmitDisabled, isFormDisabled, errorText} = this.state;
 
     return (
-      <form onSubmit={this._handleSubmit} className="reviews__form form" action="#" method="post">
+      <form onSubmit={this.handleSubmit} ref={this.formRef} className="reviews__form form" action="#" method="post">
         <label className="reviews__label form__label" htmlFor="review">Your review</label>
-        <div className="reviews__rating-form form__rating" onChange={this.handleChange}>
-          <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
+        <div onChange={this.handleChange} className="reviews__rating-form form__rating" >
+          <input disabled={isFormDisabled} className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
           <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
             </svg>
           </label>
 
-          <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
+          <input disabled={isFormDisabled} className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
           <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
             </svg>
           </label>
 
-          <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
+          <input disabled={isFormDisabled} className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
           <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
             </svg>
           </label>
 
-          <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
+          <input disabled={isFormDisabled} className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
           <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
             </svg>
           </label>
 
-          <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
+          <input disabled={isFormDisabled} className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
           <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
             <svg className="form__star-image" width="37" height="33">
               <use xlinkHref="#icon-star"></use>
             </svg>
           </label>
         </div>
-        <textarea className="reviews__textarea form__textarea" value={this.state.text} onChange={this.handleChange} id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+        <textarea disabled={isFormDisabled} value={this.state.text} onChange={this.handleChange} className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
         <div className="reviews__button-wrapper">
           <p className="reviews__help">
             To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
           </p>
-          <button className="reviews__submit form__submit button" type="submit" disabled={submitState ? `` : `disabled`}>Submit</button>
+          <button className="reviews__submit form__submit button" type="submit" disabled={isSubmitDisabled}>Submit</button>
         </div>
+        {errorText &&
+          <div style={{marginTop: `15px`, fontSize: `15px`, fontWeight: `bold`, color: `#ff0000`}}>{errorText}</div>
+        }
       </form>
     );
   }
