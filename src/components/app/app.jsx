d74bb286035470e6623e73b8db8c45e9905bd36a
Sergey <dushechkin_serge@mail.ru>
2020-07-23 16:14:49 +0300

    Установит и подключит модуль history. Подготовит App.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,69 +1,39 @@
 import React from "react";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {Router, Route, Switch} from "react-router-dom";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 
 import {ActionCreator as DataActionCreator} from "../../reducer/data/data.js";
 import {getShowAuthPage} from "../../reducer/app/selectors.js";
 import {getOffers, getActiveOfferId} from "../../reducer/data/selectors.js";
+import history from "../../history.js";
 
 import withActiveItem from "../../hocs/with-active-item/with-active-item.js";
 import Main from "../main/main.jsx";
-import Property from "../property/property.jsx";
+// import Property from "../property/property.jsx";
 import SignIn from "../sign-in/sign-in.jsx";
 
 const MainWrapped = withActiveItem(Main);
 
 class App extends React.PureComponent {
-  _renderMainScreen() {
-    const {offers, onChangeActiveOfferId, offerId, showAuth} = this.props;
-
-    if (showAuth) {
-      return (
-        <SignIn />
-      );
-    }
-
-    if (offerId === -1) {
-      return (
-        <MainWrapped
-          initActiveItemId={-1}
-          onPlaceCardHeaderClick = {onChangeActiveOfferId}
-        />
-      );
-    } else {
-      return (
-        <Property
-          offerId = {offerId}
-          offers = {offers}
-          onPlaceCardHeaderClick = {onChangeActiveOfferId}
-        />
-      );
-    }
-  }
-
   render() {
-    const {offers, onChangeActiveOfferId} = this.props;
+    const {onChangeActiveOfferId} = this.props;
 
     return (
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Route exact path="/">
-            {this._renderMainScreen()}
-          </Route>
-          <Route exact path="/property">
-            <Property
-              offerId = {1}
-              offers = {offers}
+            <MainWrapped
+              initActiveItemId={-1}
               onPlaceCardHeaderClick = {onChangeActiveOfferId}
             />
           </Route>
-          <Route exact path="/signin">
+          <Route exact path="/login">
             <SignIn
             />
           </Route>
         </Switch>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
